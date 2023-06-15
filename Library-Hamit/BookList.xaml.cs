de72830445477fac08158c40f095814cf0887d67
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-06-14 21:58:36 -0400

    Designed logic to navigate to Library Page

---
+++ Library-Hamit/BookList.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Library_Hamit
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class BookList : ContentPage
+    {
+    public BookList()
+        {
+        InitializeComponent();
+        }
+    }
+}

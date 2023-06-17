594e6377ebafe71169a1a427a8f8da9dafcffa4b
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-06-16 23:11:37 -0400

    Fix Pop-up alerts

--- Library-Hamit/BookList.xaml.cs
+++ Library-Hamit/BookList.xaml.cs
@@ -8,20 +8,120 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Library_Hamit
-{
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class BookList : ContentPage
     {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BookList : ContentPage
+        {
+        private User person;
         protected override async void OnAppearing()
             {
             base.OnAppearing();
-            collectionView.ItemsSource = await App.Database.GetBooksAsync();
+            listOfBooks.ItemsSource = await App.Database.GetBooksAsync();
             }
         public BookList(string username)
-        {
-        InitializeComponent();
+            {
+            InitializeComponent();
+            person = new User(username);
+            user.Text = $"Welcome {username}!";
+            }
+        async void Clicked_Borrowed(object sender, EventArgs e)
+            {
+            System.Console.WriteLine($"Current User is: {person.Name}");
 
-            user.Text = $"Hi {username}!, How are youu??";
+            // Handle the Borrow action
+            MenuItem menuItem = (MenuItem)sender;
+            var selectedBook = (menuItem.BindingContext as Book)?.Title;
+            if (selectedBook != null)
+                {
+
+                Book book = await App.Database.GetBookByTitle(selectedBook);
+                if (book != null && !book.IsBorrowed)
+                    {
+                    // book is available for borrowing
+
+                    book.IsBorrowed = true;
+                    book.Borrower = person.Name;
+
+                    // Update the book in the database
+                    await App.Database.UpdateBook(book);
+
+
+                    // show pop up with Operatin Successful Message
+                    await DisplayAlert("Successful", "You have borrowed the book", "OK");
+                    }
+                else
+                    {
+                    // show pop up with Operation Failed Message
+                    await DisplayAlert("Failed", "You can't borrow this book", "OK");
+                    }
+                }
+            }
+
+        async void Clicked_Return(object sender, EventArgs e)
+            {
+            System.Console.WriteLine($"Current User is: {person.Name}");
+            // Handle the Return action
+            MenuItem menuItem = (MenuItem)sender;
+            var SelectedBook = (menuItem.BindingContext as Book)?.Title;
+
+            if (SelectedBook != null)
+                {
+                Book book = await App.Database.GetBookByTitle(SelectedBook);
+                if (book != null && book.Borrower.Equals(person.Name))
+                    {
+                    // Pop-up with Operation is successful
+                    book.IsBorrowed = false;
+                    book.Borrower = "";
+                    await App.Database.UpdateBook(book);
+                    await DisplayAlert("Successful", "Book is Returned", "OK");
+                    }
+                else
+                    {
+                    // Pop-up with Operation Failed
+                    await DisplayAlert("Failed", "You can't return the Book", "OK");
+                    }
+                }
+            }
+        async void OnItemsSelected(object sender, SelectedItemChangedEventArgs e)
+            {
+            System.Console.WriteLine($"The current user is: {person.Name}");
+            if (e.SelectedItem == null)
+                {
+                return;
+                }
+
+            bookStatus.Text = "";
+
+            System.Console.WriteLine("Item Selected: ", e.SelectedItemIndex);
+
+            // retrieve the selected item from ListView
+            var selectedBook = (Book)e.SelectedItem;
+
+            if (selectedBook != null)
+                {
+                // retrieve the selected item from database
+                Book book = await App.Database.GetBookByTitle(selectedBook.Title);
+
+                // check if retrieved book is borrowed or not
+                if (book.IsBorrowed)
+                    {
+                    if (book.Borrower.Equals(person.Name))
+                        {
+                        System.Console.WriteLine($"You Checked out this Book!");
+                        bookStatus.Text = "You Checked out this Book!";
+                        }
+                    else
+                        {
+                        System.Console.WriteLine($"The Book is checked out by {book.Borrower}");
+                        bookStatus.Text = $"The Book is checked out by {book.Borrower}";
+                        }
+                    }
+                else
+                    {
+                    System.Console.WriteLine("The Book is available for borrowing!");
+                    bookStatus.Text = $"The Book is available for borrowing!";
+                    }
+                }
+            }
         }
     }
-}

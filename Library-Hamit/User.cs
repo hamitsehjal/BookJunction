037f8a1e3c4ef2d3ca8fd448f6402e7691b57c60
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-06-16 23:12:26 -0400

    Fix status of books

---
+++ Library-Hamit/User.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library_Hamit
+{
+   public class User
+    {
+        public string Name { get; set; }
+        public User(string name)
+            {
+               Name = name;
+            }
+    }
+}
